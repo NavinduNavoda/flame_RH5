78312cc8c9d401bc98ac19501b2d77ebb4b26dba
naviduNavoda <148700348+NavinduNavoda@users.noreply.github.com>
2024-04-21 08:32:57 +0530

    sa

--- back/db/db.js
+++ back/db/db.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-export async function connectToMongoDB() {
+export async function connectToMongoDB(uri) {
     try {
-        await mongoose.connect('mongodb+srv://dexterteam:sundisk123@real.lnnrwpn.mongodb.net/?retryWrites=true&w=majority&appName=real');
+        await mongoose.connect(uri);
         console.log("[+] Successfully connected to MongoDB Atlas");
     } catch (error) {
         console.error("[-] Error connecting to MongoDB Atlas:", error);
