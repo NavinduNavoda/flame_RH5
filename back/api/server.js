598fe8494608559303b6c1cd45c7762a28a38e9d
naviduNavoda <148700348+NavinduNavoda@users.noreply.github.com>
2024-04-21 08:47:02 +0530

    s

--- back/api/server.js
+++ back/api/server.js
@@ -13,7 +13,11 @@ const port = 4000;
 export async function startServer(secret) {
 
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }));
 
 
     app.use((req, res, next) => {
