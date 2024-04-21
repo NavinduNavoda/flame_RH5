fa2f9ab02d9dc76ed119064b3438537387b12370
Nimesh <110623261+DhananjayaYN@users.noreply.github.com>
2024-04-21 07:32:29 +0530

    ,l,l

--- front/interview-manager/src/App.jsx
+++ front/interview-manager/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Queue from './Queue'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        Hello world!
+        <Queue />
       </div>
     </>
   )
---
+++ front/interview-manager/src/Queue.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Scrollbar.css'; 
+const Queue = () => {
+  return (
+    <div className="scroll-container">
+      {
+        <p>shabijsdj</p>
+      }
+      <div className="content">
+        {/* Your content goes here */}
+      </div>
+    </div>
+  );
+};
+
+export default Queue;
